712a8fdf145bc8714f33ae84826c54587a2c1c39
Thu Dang <thudang.dtt@gmail.com>
2023-01-08 11:32:50 -0600

    remove animation, work with us CTA for mobile

--- script.js
+++ script.js
@@ -1,61 +1,66 @@
-let TxtType = function (el, toRotate, period) {
-  this.toRotate = toRotate;
-  this.el = el;
-  this.loopNum = 0;
-  this.period = parseInt(period, 10) || 2000;
-  this.txt = "";
-  this.tick();
-  this.isDeleting = false;
-};
 
-TxtType.prototype.tick = function () {
-  let i = this.loopNum % this.toRotate.length;
-  let fullTxt = this.toRotate[i];
 
-  if (this.isDeleting) {
-    this.txt = fullTxt.substring(0, this.txt.length - 1);
-  } else {
-    this.txt = fullTxt.substring(0, this.txt.length + 1);
-  }
+// ==============Typetext
+// let TxtType = function (el, toRotate, period) {
+//   this.toRotate = toRotate;
+//   this.el = el;
+//   this.loopNum = 0;
+//   this.period = parseInt(period, 10) || 2000;
+//   this.txt = "";
+//   this.tick();
+//   this.isDeleting = false;
+// };
 
-  this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
+// TxtType.prototype.tick = function () {
+//   let i = this.loopNum % this.toRotate.length;
+//   let fullTxt = this.toRotate[i];
 
-  let that = this;
-  let delta = 200 - Math.random() * 100;
+//   if (this.isDeleting) {
+//     this.txt = fullTxt.substring(0, this.txt.length - 1);
+//   } else {
+//     this.txt = fullTxt.substring(0, this.txt.length + 1);
+//   }
 
-  if (this.isDeleting) {
-    delta /= 2;
-  }
+//   this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
 
-  if (!this.isDeleting && this.txt === fullTxt) {
-    delta = this.period;
-    this.isDeleting = true;
-  } else if (this.isDeleting && this.txt === "") {
-    this.isDeleting = false;
-    this.loopNum++;
-    delta = 500;
-  }
+//   let that = this;
+//   let delta = 200 - Math.random() * 100;
 
-  setTimeout(function () {
-    that.tick();
-  }, delta);
-};
+//   if (this.isDeleting) {
+//     delta /= 2;
+//   }
 
-window.onload = function () {
-  let elements = document.getElementsByClassName("typewrite");
-  for (let i = 0; i < elements.length; i++) {
-    let toRotate = elements[i].getAttribute("data-type");
-    let period = elements[i].getAttribute("data-period");
-    if (toRotate) {
-      new TxtType(elements[i], JSON.parse(toRotate), period);
-    }
-  }
-  // INJECT CSS
-  let css = document.createElement("style");
-  css.innerHTML = ".typewrite > .wrap { border-right: 1px solid #000}";
-  document.body.appendChild(css);
-};
+//   if (!this.isDeleting && this.txt === fullTxt) {
+//     delta = this.period;
+//     this.isDeleting = true;
+//   } else if (this.isDeleting && this.txt === "") {
+//     this.isDeleting = false;
+//     this.loopNum++;
+//     delta = 500;
+//   }
 
+//   setTimeout(function () {
+//     that.tick();
+//   }, delta);
+// };
+
+// window.onload = function () {
+//   let elements = document.getElementsByClassName("typewrite");
+//   for (let i = 0; i < elements.length; i++) {
+//     let toRotate = elements[i].getAttribute("data-type");
+//     let period = elements[i].getAttribute("data-period");
+//     if (toRotate) {
+//       new TxtType(elements[i], JSON.parse(toRotate), period);
+//     }
+//   }
+//   // INJECT CSS
+//   let css = document.createElement("style");
+//   css.innerHTML = ".typewrite > .wrap { border-right: 1px solid #000}";
+//   document.body.appendChild(css);
+// };
+
+
+// ===========Team swiper
 // let swiper = new Swiper(".mySwiper", {
 //   loop: true,
 //   centeredSlides: true,
